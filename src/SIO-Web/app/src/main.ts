023500f746200966caa953b647da84bf83be3b38
Matt Hope <matthew.hope396@outlook.com>
2019-10-04 07:58:05 +0100

    Added sentry integration. (#19)

--- src/SIO-Web/app/src/main.ts
+++ src/SIO-Web/app/src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import App from './App.vue';
 import router from './router';
 import Store from './store';
@@ -10,6 +12,11 @@ Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 const store = new Vuex.Store(new Store());
+
+Sentry.init({
+  dsn: 'https://d062d842a1274a65b7bd9479ce74b1b6@sentry.io/1758158',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 
 new Vue({
   router,
