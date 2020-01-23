cee547723e0022c51191afce3f834e6b6446152d
Matt Hope <matthew.hope396@outlook.com>
2020-01-23 14:01:23 +0000

    WIP: documents page.

---
+++ src/SIO-Web/app/src/components/nav-sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+        <b-menu>
+            <b-menu-list label="Menu">
+                <b-menu-item icon="information-outline" label="Info"></b-menu-item>
+            </b-menu-list>
+        </b-menu>  
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+
+@Component
+export default class NavSideBar extends Vue {}
+</script>
--- src/SIO-Web/app/src/layouts/authorized.vue
+++ src/SIO-Web/app/src/layouts/authorized.vue
@@ -1,18 +1,31 @@
 <template>
 <div>
     <navbar />
-    <main role="main" class="has-padding-top-4 has-padding-left-4 w-100">
-        <slot />
-    </main>
+    <div class="has-padding-4 columns">
+        <div class="column is-one-quarter">
+            <div class="box">
+                <navsidebar/>
+            </div>        
+        </div>
+        <div class="column">
+            <div class="box">
+                <main role="main">
+                    <slot />           
+                </main>
+            </div>
+        </div>                
+    </div>    
 </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import navbar from '@/components/nav-bar.vue';
+import navsidebar from '@/components/nav-sidebar.vue';
 @Component({
     components: {
         navbar,
+        navsidebar
     },
 })
 export default class AuthorizedLayout extends Vue {}
